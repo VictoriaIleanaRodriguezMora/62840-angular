b4c0f6c8c36be27a9ebc7313a5fde5114d685456
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-12-18 20:47:46 -0300

    feat: Typescript

--- clase03_typescript/index.ts
+++ clase03_typescript/index.ts
@@ -68,3 +68,94 @@ sumar(5, 5);
 const restar: (a: number, b: number) => number = (a: number, b: number) => {
     return a - b
 }
+
+/* Tipar objetos */
+// No es practico, si tiene muchas propiedades es incomodisimo
+// Para no tener que hacer eso, existen las interfaces
+const superheroe: { nombre: string, edad: number } = {
+    nombre: "Cade Yager",
+    edad: 40
+}
+
+/* Interfaces */
+// Son un tipo de dato que yo puedo definir, para generar un contrato entre la interfaz y el objeto 
+// Los tipos de datos personalizados empiezan con mayuscula
+interface Superheroe {
+    nombre: string,
+    edad: number,
+    superpoder?: string[]
+}
+
+const superheroe_1: Superheroe = {
+    nombre: "Cinco",
+    edad: 58,
+    superpoder: ["Viajar en el tiempo", "teletransportarse en el espacio"]
+}
+
+/* Si tengo que definir muchos objetos del mismo tipo, es mejor usar una Clase  */
+
+
+class Producto {
+    precio: number;
+    marca: string;
+    id: number;
+    // Si no asignara valores en la funcion, daria error porque no tienen asignado ningun valor. Al asignar el valor del constructor, le estoy asignando el valor que paso cuando instancio un objeto de la clase
+    constructor(precio: number, marca: string, id: number) {
+        this.precio = precio;
+        this.marca = marca;
+        this.id = id;
+    }
+}
+
+const ps5 = new Producto(999, "Sony", 1)
+
+class Carrito {
+    productos: Producto[] = [];
+
+    listarProductos(): void {
+        console.log(this.productos);
+    }
+
+    agregarProductos(producto: Producto): void {
+        this.productos.push(producto)
+    }
+}
+
+class Ecommerce {
+    productos: Producto[] = [ps5]
+    carrito = new Carrito()
+}
+
+const app = new Ecommerce();
+app.carrito.agregarProductos(ps5)
+
+/* Angular está diseñado a partir de POO. Todos los componentes de un proyecto de Angular son Clases */
+// Las Clases tienen modificadores de acceso. Es la definicion que le doy a Typescript sobre como quiero exponer esa propiedad. Publica o privada 
+
+// Si no las defino de la forma corta, por defecto todas las propiedades son public
+class Persona {
+    constructor(
+        public nombre: string,
+        public apellido: string,
+    ) { }
+}
+
+class Persona2 {
+    constructor(
+        public nombre: string,
+        private apellido: string,
+    ) { }
+}
+
+
+/* Las clases y las interfaces puede trabajar en conjunto  */
+
+interface Animal {
+    cantidadPatas: number;
+    tienePelo: boolean;
+}
+
+class Perro implements Animal {
+
+}
+
