a554d89bcd952e71e5a3f94bb23f33bcb1d5bc5b
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-12-21 20:18:00 -0300

    feat: Primeros 30 mins de la clase

--- clase04_interpolacion_directivas/src/app/app.component.ts
+++ clase04_interpolacion_directivas/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'clase04_interpolacion_directivas';
+  hayError: boolean = true;
+
+  estadoEnvio: 'pendiente' | 'entregado' | 'rechazado' = 'pendiente'
+  /* estadoEnvio: 'pendiente' | 'entregado' | 'rechazado' = 'jhgfhjf' da error porque tiene que ser de alguno de esos tipos */
+
+  constructor() {
+    setInterval(() => {
+      this.hayError = !this.hayError
+    }, 1000)
+  }
 }
