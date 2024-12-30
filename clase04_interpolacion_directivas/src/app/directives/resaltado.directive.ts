9f2195a72a9da46d5619dd30f0d9063a87eb09ab
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-12-30 12:50:18 -0300

    feat: Directivas Personalizadas

--- clase04_interpolacion_directivas/src/app/directives/resaltado.directive.ts
+++ clase04_interpolacion_directivas/src/app/directives/resaltado.directive.ts
@@ -6,6 +6,11 @@ import { Directive, ElementRef } from '@angular/core';
 export class ResaltadoDirective {
   // El tipo de argumento es ElementRef
   constructor(private elementRef: ElementRef) {
-    console.log(elementRef);
+
+    console.log(elementRef);// esto es la referencia al elemento del DOM. Es el objeto del DOM que yo conozco.
+    // elementRef > nativeElement > acá estan todas las propiedades del dom que conozco. Acá opero con el DOM
+    // style.prop
+    elementRef.nativeElement.style.backgroundColor = 'purple'
   }
+  
 }
