b8287cc911422df35bde7fcded59a36c172fba19
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2025-01-17 16:51:03 -0300

    feat: traigo el proyecto base

---
+++ clase08/src/app/modules/dashboard/components/toolbar/toolbar.component.ts
@@ -0,0 +1,13 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: false,
+  
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
+})
+export class ToolbarComponent {
+  // Los output siempre son de tipo EventEmitter
+  @Output() myDrawerToggle = new EventEmitter();
+}
