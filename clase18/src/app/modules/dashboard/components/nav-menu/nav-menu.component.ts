6d2bee9f483933391a575519f93d2e6d3681925f
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2025-03-06 11:36:28 -0300

    feat: No se muestra item Usuarios con rol empleado

--- clase18/src/app/modules/dashboard/components/nav-menu/nav-menu.component.ts
+++ clase18/src/app/modules/dashboard/components/nav-menu/nav-menu.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
 import { AuthService } from '../../../../core/services/auth.service';
+import { Observable } from 'rxjs';
+import { User } from '../../../../interfaces/user';
 
 @Component({
   selector: 'app-nav-menu',
@@ -10,36 +12,41 @@ import { AuthService } from '../../../../core/services/auth.service';
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.scss'
 })
-export class NavMenuComponent {
-  constructor(
-    private router: Router,
-    private authService: AuthService
-  ) { }
+export class NavMenuComponent implements OnInit {
+  authUser$: Observable<User | null>;
+  role: string | null = null; // Definir la variable role
 
-  linktems: { label: string; routerLink: string }[] = [
-    {
-      label: "Inicio",
-      routerLink: "home",
-    },
-    {
-      label: "Estudiantes",
-      routerLink: "students",
-    },
-    {
-      label: "Cursos",
-      routerLink: "courses",
-    },
-    {
-      label: "Usuarios",
-      routerLink: "users",
-    },
-    {
-      label: "Inscripciones",
-      routerLink: "enrollments",
-    }
+  linktems = [
+    { label: 'Inicio', routerLink: 'home' },
+    { label: 'Estudiantes', routerLink: 'students' },
+    { label: 'Cursos', routerLink: 'courses' },
+    { label: 'Inscripciones', routerLink: 'enrollments' }
   ];
 
+  constructor(private authService: AuthService) {
+    this.authUser$ = this.authService.authUser$;
+  }
+
+  ngOnInit(): void {
+    this.authUser$.subscribe((user: User | null) => {
+      this.role = user?.role?.toLowerCase() || null; // Convertir el rol a minúsculas
+  
+      this.linktems = [
+        { label: 'Inicio', routerLink: 'home' },
+        { label: 'Estudiantes', routerLink: 'students' },
+        { label: 'Cursos', routerLink: 'courses' },
+        { label: 'Inscripciones', routerLink: 'enrollments' },
+      ];
+  
+      if (this.role !== 'employee') {
+        this.linktems.push({ label: 'Usuarios', routerLink: 'users' });
+      }
+  
+      console.log("Rol del usuario:", this.role); // Verificar en consola
+    });
+  }
+  
   logout(): void {
-    this.authService.logout()
+    this.authService.logout();
   }
 }
